4b75a8e8d417563cee004d030022a8ffabaedacd
modus.operandi <n.plaksin@technologysv.ru>
2024-04-26 12:52:36 +0300

    add generated assets

--- src/media/audio/pulseaudio/pulselayer.cpp
+++ src/media/audio/pulseaudio/pulselayer.cpp
@@ -459,7 +459,7 @@ PulseLayer::startStream(AudioDeviceType type)
             createStream(record_, type, *dev_infos, ec, std::bind(&PulseLayer::readFromMic, this));
 
             // whenever the stream is moved, it will call this cb
-            record_->setEchoCancelCb([this](bool echoCancel) { setHasNativeAEC(echoCancel); });
+            record_->setEchoCancelCb([this](bool echoCancel) { });
         }
     }
     pa_threaded_mainloop_signal(mainloop_.get(), 0);
