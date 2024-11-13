147ddac89944e280a4145701830b91f3d8ba1fae
modus.operandi <modus.operandi@mail.com>
2024-11-13 11:24:09 +0300

    chore: remove unused macro

--- src/sip_core/def.h
+++ src/sip_core/def.h
@@ -32,20 +32,18 @@
 #define LIBSIP_CORE_HIDDEN __attribute__((visibility("hidden")))
 #endif
 
-// Now we use the generic helper definitions above to define LIBSIP_CORE_PUBLIC and LIBSIP_CORE_LOCAL.
-// LIBSIP_CORE_PUBLIC is used for the public API symbols. It is either DLL imports or DLL exports (or does
-// nothing for static build) LIBSIP_CORE_LOCAL is used for non-api symbols.
+// Now we use the generic helper definitions above to define LIBSIP_CORE_PUBLIC and
+// LIBSIP_CORE_LOCAL. LIBSIP_CORE_PUBLIC is used for the public API symbols. It is either DLL
+// imports or DLL exports (or does nothing for static build)
 
 #ifdef SIP_CORE_EXPORTS // defined if sip_core is compiled as a shared library
-#ifdef LIBSIP_CORE_BUILD  // defined if we are building the sip_core shared library (instead of using it)
+#ifdef LIBSIP_CORE_BUILD // defined if we are building the sip_core shared library (instead of using it)
 #define LIBSIP_CORE_PUBLIC LIBSIP_CORE_EXPORT
 #else
 #define LIBSIP_CORE_PUBLIC LIBSIP_CORE_IMPORT
 #endif // LIBSIP_CORE_BUILD
-#define LIBSIP_CORE_LOCAL LIBSIP_CORE_HIDDEN
-#else // SIP_CORE_EXPORTS is not defined: this means sip_core is a static lib.
+#else  // SIP_CORE_EXPORTS is not defined: this means sip_core is a static lib.
 #define LIBSIP_CORE_PUBLIC
-#define LIBSIP_CORE_LOCAL
 #endif // SIP_CORE_EXPORTS
 
 #ifdef DEBUG
