77d128dedfb70e59fd859760f0f334c0cc4e544a
modus.operandi <n.plaksin@technologysv.ru>
2025-05-26 17:49:36 +0300

    fix: DTMF events as in RFC

--- src/call.h
+++ src/call.h
@@ -389,7 +389,7 @@ public:
      * Send DTMF
      * @param code  The char code
      */
-    virtual void carryingDTMFdigits(const std::string& dtmfEvents) = 0;
+    virtual void carryingDTMFdigits(const std::string& dtmfEvents, double duration, unsigned int volume) = 0;
 
     /**
      * Make a change request of the current media with the provided media
