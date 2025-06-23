9d3ed205e30bc58b3261452b5fc5b5ce489e9018
kirill.yakovlev <k.yakovlev@svetets.ru>
2025-06-23 14:56:38 +0300

    fix: desktop sharing recorder doesnt work properly

--- src/media/video/video_device.h
+++ src/media/video/video_device.h
@@ -45,7 +45,7 @@ namespace video {
 using VideoSize = std::pair<unsigned, unsigned>;
 using FrameRate = rational<double>;
 static constexpr const char DEVICE_DESKTOP[] = "desktop";
-static constexpr int DESKTOP_FRAME_RATE = 10;
+static constexpr int DESKTOP_FRAME_RATE = 30;
 
 class VideoDeviceImpl;
 
