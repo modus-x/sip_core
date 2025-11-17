1f3422b4b9784bb5f4ed4a062b83daf66fb4144d
modus.operandi <modus.operandi@mail.com>
2025-11-17 14:26:10 +0300

    chore: update version to v0.14.21

--- example/sip_cli/src/main.cpp
+++ example/sip_cli/src/main.cpp
@@ -25,8 +25,8 @@
 // getPassword();
 
 std::string username = "dev_user";
-std::string password = "!QAZxsw2";
-std::string domain = "192.168.92.27";
+std::string password = "12345";
+std::string domain = "192.168.92.43";
 
 std::atomic_bool g_needFinish(false);
 std::queue<std::vector<std::string>> g_command_queue;
@@ -40,7 +40,7 @@ std::vector<std::string> split(const std::string& s);
 int
 main()
 {
-    std::cout << "SIP core Console App" << std::endl;
+            std::cout << "SIP core Console App" << std::endl;
     std::cout << "Available commands: call <callee>, add <callee>, del <callee>, move <from> <to>, "
                  "conf <callee1> ... <calleeN>, switch <device>, hold, resume, hangup, capOn, capOff, video, reregister, unregister, subscribe, unsubscribe, publish, exit"
               << std::endl;
@@ -271,7 +271,7 @@ main()
             for (auto it = info.begin(); it != info.end(); ++it) {
                 std::cout << " " << it->first << " : " << it->second << std::endl;
             }
-} else if (command == "unregister") {
+        } else if (command == "unregister") {
             if (!controller.unregister()) {
                 std::cerr << "Error: unable to send unregister for current account." << std::endl;
             } else {
