d7a3bff4502474756400fabdb1681ce97f9fcef2
kirill.yakovlev <k.yakovlev@svetets.ru>
2026-02-11 19:32:07 +0300

    chore: windows build script now excludes VS 2026 from search path

--- compat/msvc/winmake.py
+++ compat/msvc/winmake.py
@@ -86,6 +86,7 @@ def getLatestVSVersion():
     args = [
         '-latest',
         '-products *',
+        '-version [16.0,17.99]',
         '-requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64',
         '-property installationVersion'
     ]
@@ -133,6 +134,7 @@ def findVSLatestDir():
     args = [
         '-latest',
         '-products *',
+        '-version [16.0,17.99]',
         '-requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64',
         '-property installationPath'
     ]
